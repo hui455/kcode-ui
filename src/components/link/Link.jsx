7c6cef6dcb3f948f6984424193cce1f2a0b7a5e5
徐455 <12144663+hui455@user.noreply.gitee.com>
2024-04-29 02:22:30 +0800

    4

--- src/components/link/Link.jsx
+++ src/components/link/Link.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import './Link.scss';
 import '../Type.css'
-export default function KLink({ children,type = 'default',underline,disabled,onClick,className,...props}) {
-   let klink = 'link-'+ type;
-  const kadd = (a)=>{  klink = klink + ' ' + a;}
-  if(underline)kadd('underline')
+export default function KLink({ children,type = 'default',style,underline,disabled,onClick,...props}) {
+
   if(disabled){
-    kadd('disabled');
     onClick = (e)=>{e.preventDefault()}
   }
   return (
-    <a className={'link ' + klink +' '+ className} onClick={onClick} {...props} >
+    <a className={`k-link ${underline?'underline':''} ${disabled?'disabled':''} ${type === 'default' ? type :''}`} style={{...style,'--k-link': `var(--el-color-${type})`}} onClick={onClick} {...props} >
       {children}
     </a>
   );
